6f521b3fa216256da1354249e67dcb5ac3bc6fd4
rupert@it031740 <rupertcollins@gmail.com>
2021-06-08 12:59:15 +0100

    changed to https

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,18 +3,21 @@
 #################################################### 
 #################################################### 
 
-
-# DOWNLOAD REFSEQ
-wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/RELEASE_NUMBER -P temp
-head temp/RELEASE_NUMBER
-wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/README -P temp
-wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.1.1.genomic.fna.gz -P temp
-wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.2.1.genomic.fna.gz -P temp
-# check the release number
+# download refseq version
+wget -q https://ftp.ncbi.nlm.nih.gov/refseq/release/RELEASE_NUMBER -P temp
 file="temp/RELEASE_NUMBER"
 gbv=$(cat "$file")
-echo "Downloaded RefSeq version ""$gbv"
-wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/release-catalog/RefSeq-release"$gbv".catalog.gz -P temp
+echo "Downloading RefSeq version ""$gbv"" ..."
+sleep 3
+
+# download readme and DNA seqs
+wget -q https://ftp.ncbi.nlm.nih.gov/refseq/release/README -P temp
+wget -q https://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.1.1.genomic.fna.gz -P temp
+wget -q https://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.2.1.genomic.fna.gz -P temp
+
+# download 
+wget -q https://ftp.ncbi.nlm.nih.gov/refseq/release/release-catalog/RefSeq-release"$gbv".catalog.gz -P temp
+printf "...\nGetting catalogue md5sum\n"
 md5sum temp/RefSeq-release"$gbv".catalog.gz
 gzip -cd temp/RefSeq-release"$gbv".catalog.gz | grep "mitochondrion" > temp/refseq.mitochondrion.cat.tsv
 rm temp/RefSeq-release"$gbv".catalog.gz
@@ -30,5 +33,4 @@ rm temp/mitochondrion.1.1.genomic.fna temp/mitochondrion.2.1.genomic.fna
 # clean
 sed -i -e 's/ .*//g' temp/refseq.mitochondrion.genomic.fna
 
-# check 
-#head temp/refseq.mitochondrion.genomic.fna
+printf "...\nRefSeq DNA and catalogue written to 'temp'.\n"
